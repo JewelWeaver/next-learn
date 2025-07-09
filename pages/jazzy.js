3d4aa094ab63f00c6756a0c025973094be74c76c
Jude Evangeline <jude@emeraldcitybeacon.org>
2025-07-09 15:24:47 -0700

    Update jazzy.js

--- pages/jazzy.js
+++ pages/jazzy.js
@@ -5,11 +5,6 @@ export default function JazzyPage() {
     <div style={{ padding: "2rem", fontFamily: "serif", background: "#fffafc" }}>
       <h1 style={{ color: "#aa336a", fontSize: "2.5rem" }}>All Hail Queen Jazzy 👑</h1>
       
-      <img 
-        src="https://placekitten.com/300/300" 
-        alt="Queen Jazzy" 
-        style={{ borderRadius: "50%", margin: "1rem 0", border: "4px solid #aa336a" }} 
-      />
 
       <h2>Jazzy’s Favorite Things:</h2>
       <ul>
